9146e8635a210c275ee5e89891aec5fb40a9b3b3
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-03-04 22:00:33 +0300

    Update PythonApplication7.py

--- PythonApplication7/PythonApplication7/PythonApplication7.py
+++ PythonApplication7/PythonApplication7/PythonApplication7.py
@@ -4,7 +4,7 @@ def Cost_Of_Call(City, Call_Duration):
         return 0
     elif(City == 381):
         Cost_Of_Call_1 = 18
-    elif(City = 473):
+    elif(City == 473):
         Cost_Of_Call_1 = 13
     else:
         Cost_Of_Call_1 = 11
@@ -13,4 +13,5 @@ def Cost_Of_Call(City, Call_Duration):
     print(Cost_Of_Call)
     return 0
 City = int(input('\u0412\u0432\u0435\u0434\u0438\u0442\u0435\u0020\u043a\u043e\u0434\u0020\u0433\u043e\u0440\u043e\u0434\u0430\u002e\n'))
+
 Call_Duration = int(input('\u0412\u0432\u0435\u0434\u0438\u0442\u0435\u0020\u0434\u043b\u0438\u0442\u0435\u043b\u044c\u043d\u043e\u0441\u0442\u044c\u0020\u0440\u0430\u0437\u0433\u043e\u0432\u043e\u0440\u0430\u002e\n'))
